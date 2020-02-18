d9fa15ea5d229d7c9c8245772aae2c67b22db6c9
Zhanna Kresteva <kresteva.astana@gmail.com>
2020-02-18 23:12:38 +0200

    Fetching data without caching

--- js/db.js
+++ js/db.js
@@ -2,9 +2,10 @@
 
 db.collection('todo-items').onSnapshot((snapshot) => {
   snapshot.docChanges().forEach((change) => {
-    // console.log(change, change.doc.data(), change.doc.id)
+    console.log(change, change.doc.data(), change.doc.id)
     if(change.type === 'added'){
       // add the document data to the web page
+      renderTodo(change.doc.data(), change.doc.id);
     }
     if(change.type === 'removed'){
       // remove the document data to the web page
--- js/ui.js
+++ js/ui.js
@@ -1,3 +1,5 @@
+const todoitems = document.querySelector('.todos');
+
 document.addEventListener('DOMContentLoaded', function() {
   // nav menu
   const menus = document.querySelectorAll('.side-menu');
@@ -6,3 +8,24 @@ document.addEventListener('DOMContentLoaded', function() {
   const forms = document.querySelectorAll('.side-form');
   M.Sidenav.init(forms, {edge: 'left'});
 });
+
+//render todo data
+const renderTodo = (data, id) => {
+  const html = `
+  <div class="card-panel todo white row" data-id="${id}">
+    <label class="valign-wrapper">
+      <input type="checkbox" class="filled-in" checked="checked" />
+      <span></span>
+    </label>
+    <div class="todo-details">
+      <div class="todo-title">${data.title}</div>
+      <div class="todo-description">${data.description}</div>
+    </div>
+    <div class="todo-delete">
+      <i class="material-icons" data-id="${id}">delete_outline</i>
+    </div>
+  </div>
+  `;
+
+  todoitems.innerHTML += html;
+}
