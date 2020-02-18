2221d8458c6bfd655b76348bab0404b882bb74fc
Zhanna Kresteva <kresteva.astana@gmail.com>
2020-02-19 00:01:17 +0200

    Indexed DB. Data available offline.

--- sw.js
+++ sw.js
@@ -1,16 +1,16 @@
 const staticCacheName = 'site-static-v5';
 const dynamicCacheName = 'site-dynamic-v5';
 const assets = [
-  '/',
-  '/index.html',
-  '/js/app.js',
-  '/js/ui.js',
-  '/js/materialize.min.js',
-  '/css/styles.css',
-  '/css/materialize.min.css',
+  './',
+  './index.html',
+  './js/app.js',
+  './js/ui.js',
+  './js/materialize.min.js',
+  './css/styles.css',
+  './css/materialize.min.css',
   'https://fonts.googleapis.com/icon?family=Material+Icons',
   'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
-  '/pages/fallback.html'
+  './pages/fallback.html'
 ];
 
 // cache size limit function
@@ -51,21 +51,22 @@ self.addEventListener('activate', evt => {
 
 // fetch event
 self.addEventListener('fetch', evt => {
-  //console.log('fetch event', evt);
-  // evt.respondWith(
-  //   caches.match(evt.request).then(cacheRes => {
-  //     return cacheRes || fetch(evt.request).then(fetchRes => {
-  //       return caches.open(dynamicCacheName).then(cache => {
-  //         cache.put(evt.request.url, fetchRes.clone());
-  //         // check cached items size
-  //         limitCacheSize(dynamicCacheName, 15);
-  //         return fetchRes;
-  //       })
-  //     });
-  //   }).catch(() => {
-  //     if(evt.request.url.indexOf('.html') > -1){
-  //       return caches.match('/pages/fallback.html');
-  //     } 
-  //   })
-  // );
+  if(evt.request.url.indexOf('firestore.googleapis.com') === -1){
+    evt.respondWith(
+      caches.match(evt.request).then(cacheRes => {
+        return cacheRes || fetch(evt.request).then(fetchRes => {
+          return caches.open(dynamicCacheName).then(cache => {
+            cache.put(evt.request.url, fetchRes.clone());
+            // check cached items size
+            limitCacheSize(dynamicCacheName, 15);
+            return fetchRes;
+          })
+        });
+      }).catch(() => {
+        if(evt.request.url.indexOf('.html') > -1){
+          return caches.match('/pages/fallback.html');
+        } 
+      })
+    );
+  }
 });
