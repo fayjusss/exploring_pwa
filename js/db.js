ede6b086f0172be71619a9b2f0bc47e1b26f029c
Zhanna Kresteva <kresteva.astana@gmail.com>
2020-02-18 22:52:31 +0200

    Callback fired off on database snapshot

--- js/db.js
+++ js/db.js
@@ -2,6 +2,12 @@
 
 db.collection('todo-items').onSnapshot((snapshot) => {
   snapshot.docChanges().forEach((change) => {
-    console.log(change, change.doc.data(), change.doc.id)
+    // console.log(change, change.doc.data(), change.doc.id)
+    if(change.type === 'added'){
+      // add the document data to the web page
+    }
+    if(change.type === 'removed'){
+      // remove the document data to the web page
+    }
   })
 })
