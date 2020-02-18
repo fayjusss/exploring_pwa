51614585fb40682e3483e42d5e4278f103f64967
Zhanna Kresteva <kresteva.astana@gmail.com>
2020-02-18 13:10:53 +0200

    Fetching caches assets

--- sw.js
+++ sw.js
@@ -9,7 +9,8 @@ const assets = [
   '/css/materialize.min.css',
   '/img/dish.png',
   'https://fonts.googleapis.com/icon?family=Material+Icons',
-  'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
+  'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
+  '/pages/fallback.html'
 ];
 
 // cache size limit function
@@ -37,9 +38,28 @@ self.addEventListener('install', evt => {
 // activate event
 self.addEventListener('activate', evt => {
   //console.log('service worker activated');
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      // concole.log(keys);
+      return Promise.all(keys
+        .filter(key => key !== staticCacheName)
+        .map(key => caches.delete(key))
+        )
+    })
+  );
 });
 
 // fetch event
 self.addEventListener('fetch', evt => {
   //console.log('fetch event', evt);
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request).then(fetchRes => {
+        return caches.open(dynamicCache).then(cache => {
+          cache.put(evt.request.url, fetches.clone());
+          return fetchRes;
+        })
+      });
+    })
+  );
 });
